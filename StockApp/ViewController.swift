23390eabbf5e949348379c003fa2f4f6c5f73a62
Andrew Barth <ajba266@uky.edu>
2019-10-06 21:29:36 -0400

    Initial Commit

---
+++ StockApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StockApp
+//
+//  Created by  on 10/6/19.
+//  Copyright © 2019 Andrew Barth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
